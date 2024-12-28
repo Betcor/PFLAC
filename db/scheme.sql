2aa5140c56113e82be376724244b91018f87cf35
fxhxyz4 <fxhxyz8@gmail.com>
2024-12-28 12:48:11 +0200

    update sql scheme

--- db/scheme.sql
+++ db/scheme.sql
@@ -16,6 +16,33 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `table_physical`
+--
+
+DROP TABLE IF EXISTS `table_physical`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `table_physical` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `age_group` int DEFAULT NULL,
+  `gender` varchar(50) DEFAULT NULL,
+  `exercise_number` int DEFAULT NULL,
+  `exercise_name` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `table_physical`
+--
+
+LOCK TABLES `table_physical` WRITE;
+/*!40000 ALTER TABLE `table_physical` DISABLE KEYS */;
+INSERT INTO `table_physical` VALUES (1,40,'man',14,'Підтягування на перекладині'),(2,40,'man',25,'Біг на 100 м'),(3,40,'man',3,'Біг на 3 км'),(4,44,'man',14,'Підтягування на перекладині –ьб'),(5,44,'man',21,'Згинання та розгинання тулуба'),(6,44,'man',2,'Біг на 2 км'),(7,49,'man',14,'Підтягування на перекладині(згинання та розгинання рук в упорі лежачи)'),(8,49,'man',21,'Згинання та розгинання тулуба'),(9,49,'man',2,'Біг на 2 км'),(10,50,'man',14,'Підтягування на перекладині(згинання та розгинання рук в упорі лежачи)'),(11,50,'man',21,'Згинання та розгинання тулуба'),(12,50,'man',1,'Біг на 1 км'),(13,30,'woman',20,'Згинання та розгинання рук в упорі лежачи'),(14,30,'woman',21,'Згинання та розгинання тулуба'),(15,30,'woman',3,'Біг на 3 км'),(16,39,'woman',20,'Згинання та розгинання рук в упорі лежачи'),(17,39,'woman',21,'Згинання та розгинання тулуба'),(18,39,'woman',2,'Біг на 2 км'),(19,40,'woman',20,'Згинання та розгинання рук в упорі лежачи'),(20,40,'woman',21,'Згинання та розгинання тулуба'),(21,40,'woman',1,'Біг на 1 км');
+/*!40000 ALTER TABLE `table_physical` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `table_standarts`
 --
 
@@ -44,6 +71,10 @@ LOCK TABLES `table_standarts` WRITE;
 INSERT INTO `table_standarts` VALUES (1,1,25,85,'295/390','275/360','255/340'),(2,2,25,80,'280/370','260/340','240/320'),(3,3,25,80,'280/370','260/340','240/320'),(4,1,30,85,'295/390','275/360','255/340'),(5,2,30,75,'265/350','245/320','225/300'),(6,3,30,75,'265/350','245/320','225/300'),(7,1,35,80,'280/370','260/340','240/320'),(8,2,35,70,'250/330','230/300','210/280'),(9,3,35,70,'250/330','230/300','210/280'),(10,1,40,80,'280/370','260/340','240/320'),(11,2,40,65,'235/310','215/280','195/260'),(12,3,40,60,'220/290','200/260','180/240'),(13,1,45,65,'235/310','215/280','195/260'),(14,2,45,55,'205/270','185/240','165/220'),(15,3,45,45,'175/230','155/200','135/180'),(16,1,50,55,'205','185','165'),(17,2,50,45,'175','155','135'),(18,3,50,35,'145','125','105'),(19,1,55,45,'175','155','135'),(20,2,55,35,'145','125','105'),(21,3,55,25,'115','95','75'),(22,1,60,35,'145','125','105'),(23,2,60,25,'115','105','75'),(24,3,60,15,'85','65','45'),(25,1,70,25,'115','95','75'),(26,2,70,15,'85','65','45'),(27,3,70,5,'55','35','15');
 /*!40000 ALTER TABLE `table_standarts` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'datadb'
+--
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -54,4 +85,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-12-27 18:50:26
+-- Dump completed on 2024-12-28 12:44:57

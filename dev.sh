b56a6197ace1e24ba61a4a508273875c978d3e30
fxhxyz4 <fxhxyz8@gmail.com>
2025-01-01 19:08:31 +0200

    added last_change table to datadb & update dev.sh

--- dev.sh
+++ dev.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
 
+base_dir=$(find . -type d -name "PFLAC" -print -quit)
+file_name=".env.prod"
+
+if [ -d "$base_dir" ]; then
+    echo "Directory '$base_dir' found."
+
+    if [ -f "$base_dir/$file_name" ]; then
+        echo "Found .env.prod file at: $base_dir/$file_name"
+    else
+        echo ".env.prod file is missing in the directory."
+    fi
+else
+    echo "Directory '$base_dir' not found in the current path."
+fi
